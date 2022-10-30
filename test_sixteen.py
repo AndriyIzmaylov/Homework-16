b4282eb99af9e05897296bf10e88192e59af227d
AndriyIzmaylov <82765435+AndriyIzmaylov@users.noreply.github.com>
2022-10-30 20:24:15 +0200

    Update test_sixteen.py

--- test_sixteen.py
+++ test_sixteen.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome('/var/lib/jenkins/.local/bin/chromedriver-binary')
 new_user_name = "rando"
 new_user_email = "aa@aa.com"
 updated_name = "ololo"
